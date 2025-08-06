ea367405354ca2bdc03c9006b864ecc729c4fb93
Unagi_zoso <Unagi.zoso@gmail.com>
2025-08-06 20:42:29 +0900

    feat: 도서관 API 라우터 연결
    bss-6-impl-calculate-optimal-library-set

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import booksRouter from './books';
+import librariesRouter from './libraries';
 
 const router = Router();
 
 router.use('/books', booksRouter);
+router.use('/libraries', librariesRouter);
 
 export default router;
